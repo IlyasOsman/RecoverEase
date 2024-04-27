69c984bddd441652a296836b394cc532241de020
Kennedy Musau <kennedy.musauu@gmail.com>
2024-04-27 23:39:11 +0300

    add sign up

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,15 @@
-import {Routes, Route, Navigate} from "react-router-dom";
-import Home from "./components/Home"
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
 import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/login" element={<LogIn/>} />
+      <Route path="/login" element={<LogIn />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 };
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,9 @@
-import { useForm } from "react-hook-form";
 import "./login.css";
+
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-const LogIn = () => {
+
+function SignUp() {
   const {
     register,
     handleSubmit,
@@ -14,7 +16,7 @@ const LogIn = () => {
   return (
     <div className="login-section">
       <div className="form-header">
-        <h1>Welcome Back!</h1>
+        <h1>Create free account</h1>
       </div>
       <form className="login-form" onSubmit={handleSubmit(handleOnSubmit)}>
         <label htmlFor="username">Username</label>
@@ -43,14 +45,14 @@ const LogIn = () => {
         {errors.username && <p className="error">{errors.username.message}</p>}
         {errors.password && <p className="error">{errors.password.message}</p>}
 
-        <button type="submit">Log In</button>
+        <button type="submit">Create Account</button>
 
         <p>
-          Dont have an account yet? <Link to="/signup">Sign Up</Link>
+          Already have an account? <Link to="/login">Log In</Link>
         </p>
       </form>
     </div>
   );
-};
+}
 
-export default LogIn;
+export default SignUp;
